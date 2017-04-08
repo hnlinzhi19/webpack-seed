f33f9fd3a4a83653251d501cbfc52ae38d986f2c
MuMuShi <hnaynlin19@163.com>
2017-04-09 06:51:16 +0800

    tpl 只能用 require

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,9 @@ import '../scss/index.scss';
 console.log(isArray([1, 3]));
 console.log('111111');
 // import testTpl from './tpl/test.tpl';
+/**
+ * 必须使用require 否则会报错
+ */
 var testTpl = require('./tpl/test.tpl');
 let html =testTpl({
     synopsis:'sss'
